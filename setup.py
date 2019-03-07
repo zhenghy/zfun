3b4231fe672e51439965be52206bec2ecde3aa5c
zhenghy <wnfdsfy@gmail.com>
2019-03-07 11:10:35 +0800

    Version 0.3.6 1. update setup.py for pip type markdown . 2. update manifest.in for file README.MD . 3. update __init__.py for function amkdir .

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import setuptools
 from distutils.core import setup
-print(setuptools.find_packages())
 setup(
     name='z-py',
-    version='0.2.0',
+    version='0.3.6',
     author='Haiyang Zheng',
     author_email='wnfdsfy@gmail.com',
     packages=setuptools.find_packages(),
     url='https://github.com/zhenghy/zpy',
     license='MIT',
     description='AWM :Always With Me Function',
-    long_description=open('README.MD').read(),
+    long_description=open('README.MD','r').read(),
+    long_description_content_type='text/markdown',
     install_requires=[],
     keywords=('always', 'function'),
     platforms='any',
--- zpy/__init__.py
+++ zpy/__init__.py
@@ -1,4 +1,4 @@
 __author__ = 'zhenghy'
-__all__ = ['awm','astr','anum','nowf']
+__all__ = ['awm','astr','anum','nowf','amkdir']
 
 from zpy.awm import *
